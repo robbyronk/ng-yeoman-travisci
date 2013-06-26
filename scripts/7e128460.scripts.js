bdd1ef78c47b9545bb52f375e1ebfcc29748a0de
robbyronk <robby.ronk@gmail.com>
2013-06-26 13:48:52 +0000

    adding the yeoman build files to gh-pages [ci skip]

--- scripts/7e128460.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("ngYeomanTravisciApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("ngYeomanTravisciApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
